6188b80ddd8329cde36d3e5b182ac1d36f456d33
OlexaGames <61917242+OlexaGames@users.noreply.github.com>
2025-07-24 14:17:52 +0300

    Delete instruction-DCUg-KPKrug/service-maintenance-guide-html/auth-check.js

--- instruction-DCUg-KPKrug/service-maintenance-guide-html/auth-check.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    if (!sessionStorage.getItem('isAuthenticated')) {
-        window.location.href = '../instruction-DCUg-KPKrug/auth.html';
-    }
-});
