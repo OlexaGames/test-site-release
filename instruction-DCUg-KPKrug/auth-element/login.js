6328c5a0ace7555a7677381c964c04edaad035bc
OlexaGames <61917242+OlexaGames@users.noreply.github.com>
2025-07-25 09:51:35 +0300

    Update login.js
    update sessionsStorage.

--- instruction-DCUg-KPKrug/auth-element/login.js
+++ instruction-DCUg-KPKrug/auth-element/login.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const correctHash = '8da1b3a64be6a1b28464323237655d6cfc518c2b4930b683db273d39a9a3b601';
+    const correctHash = '0b45d3f1c5c6b8a4f0b3e8a2c9d1e0f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7';
     const form = document.getElementById('loginForm');
     const error = document.getElementById('error');
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        error.textContent = '';
-        error.style.display = 'none';
         const password = document.getElementById('password').value;
         const hashedPassword = CryptoJS.SHA256(password).toString();
 
         if (hashedPassword === correctHash) {
             sessionStorage.setItem('isAuthenticated', 'true');
-            window.location.href = '../instruction-DCUg-KPKrug/main.html';
+            const redirectUrl = sessionStorage.getItem('redirectAfterLogin') || '/test-site-release/instruction-DCUg-KPKrug/main.html';
+            window.location.href = redirectUrl;
+            sessionStorage.removeItem('redirectAfterLogin');
         } else {
             error.textContent = 'Неверный пароль!';
             error.style.display = 'block';
