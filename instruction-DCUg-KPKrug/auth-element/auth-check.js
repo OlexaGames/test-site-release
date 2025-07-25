e2c2e31a1c6205d97b04153dfa1a4aa4422b327d
OlexaGames <61917242+OlexaGames@users.noreply.github.com>
2025-07-25 09:49:08 +0300

    Update auth-check.js
    add sessionStorage.

--- instruction-DCUg-KPKrug/auth-element/auth-check.js
+++ instruction-DCUg-KPKrug/auth-element/auth-check.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     if (!sessionStorage.getItem('isAuthenticated')) {
+        sessionStorage.setItem('redirectAfterLogin', window.location.pathname);
         window.location.href = '/test-site-release/instruction-DCUg-KPKrug/auth.html';
     }
 });
