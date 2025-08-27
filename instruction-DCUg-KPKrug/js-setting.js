2ff08add201a2d40b3a1f048216377a34997d896
OlexaGames <61917242+OlexaGames@users.noreply.github.com>
2025-08-27 10:18:42 +0300

    Update js-setting.js
    add subMyFunction & showSubElementId

--- instruction-DCUg-KPKrug/js-setting.js
+++ instruction-DCUg-KPKrug/js-setting.js
@@ -11,12 +11,34 @@ function myFunction(id) {
   }
 }
 
+function subMyFunction(id) {
+  var element = document.getElementById(id);
+  if (element) {
+    element.classList.toggle("show");
+
+  if (element.classList.contains("show")) {
+  localStorage.setItem("showSubElementId", id);
+    } else {
+      localStorage.removeItem("showSubElementId");
+    }
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   var showElementId = localStorage.getItem("showElementId");
   if (showElementId) {
     var element = document.getElementById(showElementId);
     if (element) {
       element.classList.add("show");
+    }
+  }
+});
+
+  var showSubElementId = localStorage.getItem("showSubElementId");
+  if (showSubElementId) {
+    var element2 = document.getElementById(showSubElementId);
+    if (element2) {
+      element2.classList.add("show");
     }
   }
 });
@@ -31,3 +53,4 @@ document.addEventListener('DOMContentLoaded', () => {
     const currentYear = new Date().getFullYear();
     copyrightElement.textContent = `Ⓒ1992-${currentYear} ООО НПЦ «Промавтоматика»`;
 });
+
