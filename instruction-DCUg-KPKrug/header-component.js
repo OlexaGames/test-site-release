36328f96215cbc6d6cc905492f0f9c6c0bbc3e3b
OlexaGames <61917242+OlexaGames@users.noreply.github.com>
2025-11-19 12:58:44 +0300

    Улучшен файл header-component

--- instruction-DCUg-KPKrug/header-component.js
+++ instruction-DCUg-KPKrug/header-component.js
@@ -1,22 +1,39 @@
 class HeaderComponent extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = `
-            <link rel="stylesheet" href="style.css">
+    }
+
+    connectedCallback() {
+        const currentPath = window.location.pathname;
+        
+        let basePath = '';
+        
+        if (currentPath.includes('/instruction-DCUg-KPKrug/')) {
+            const pathAfterBase = currentPath.split('/instruction-DCUg-KPKrug/')[1];
+            if (pathAfterBase && pathAfterBase.includes('/')) {
+                basePath = '../';
+            } else {
+                basePath = '';
+            }
+        } else {
+            basePath = 'instruction-DCUg-KPKrug/';
+        }
+
+        this.innerHTML = `
             <header>
-                <nav class="menu">
+                <nav class="menu" id="menu">
                     <ul>
-                        <li><a href="service-maintenance.html" class="menu-element">Сервисное<br>обслуживание</a></li>
-                        <li><a href="replacing-configuring-modules.html" class="menu-element">Замена и настройка<br>модулей</a></li>
-                        <li><a href="possible-malfunctions.html" class="menu-element">Возможные<br>неисправности</a></li>
+                        <li><a href="${basePath}service-maintenance.html" class="menu-element">Сервисное<br>обслуживание</a></li>
+                        <li><a href="${basePath}replacing-configuring-modules.html" class="menu-element">Замена, настройка,<br>соединение модулей</a></li>
+                        <li><a href="${basePath}possible-malfunctions.html" class="menu-element">Возможные<br>неисправности</a></li>
                     </ul>
                 </nav>
-                <button class="burger" aria-label="Открыть меню" aria-expanded="false">
+                <button class="burger" id="burger" aria-label="Открыть меню" aria-expanded="false">
                     <span class="burger-line"></span>
                 </button>
-                <a class="image-btn" href="main.html">
-                    <img src="img-css/logo-rezerv.png" class="logo-header">
+                <a class="image-btn" href="${basePath}main.html">
+                    <img src="${basePath}img-css/logo-rezerv.png" class="logo-header">
+                    <img src="${basePath}img-css/logo.png" class="logo-header logo-mobile"> 
                 </a> 
             </header>
         `;
